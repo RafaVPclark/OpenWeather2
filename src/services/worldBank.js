1e723439fcb4bb9412643f159934cba4ee98b2ac
RafaelClark <rafael.clark@planejamento.mg.gov.br>
2024-08-18 22:32:59 -0300

    Alterações atuais

--- src/services/worldBank.js
+++ src/services/worldBank.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const worldBank = axios.create({
-       baseURL: "http://api.worldbank.org/v2/country/",
+       baseURL: "https://api.worldbank.org/v2/country/",
 });
 
 export default worldBank;
